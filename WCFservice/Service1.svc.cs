af3b75d1378005989687586ed8946683ed809975
SwimUp <koviazin.pro1@yandex.ru>
2018-11-20 17:55:36 +0500

    UPD

--- WCFservice/Service1.svc.cs
+++ WCFservice/Service1.svc.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
-using System.IO;
-using System.Net;
+﻿using System.IO;
 using System.Web;
 
 namespace WCFservice
