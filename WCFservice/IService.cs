63e3ca72794730df530843febe1b5cbf804762ca
SwimUp <koviazin.pro1@yandex.ru>
2018-11-20 17:47:10 +0500

    Init3

---
+++ WCFservice/IService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.IO;
+
+namespace WCFservice
+{
+    [ServiceContract(Namespace = "WCFservice")]
+    public interface IService
+    {
+        [OperationContract]
+        Stream DownloadFile(string fileName);
+
+        [OperationContract]
+        bool GetStatus();
+
+        [OperationContract(IsOneWay = true)]
+        void UploadFile(FileTransferRequest request);
+    }
+
+
+    [MessageContract()]
+    public class FileTransferRequest
+    {
+        [MessageHeader(MustUnderstand = true)]
+        public string FileName;
+
+        [MessageBodyMember(Order = 1)]
+        public Stream Data;
+    }
+}
---
+++ WCFservice/Service1.svc.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Web;
+
+namespace WCFservice
+{
+    public class Service1 : IService
+    {
+        public bool GetStatus()
+        {
+            return true;
+        }
+
+        public Stream DownloadFile(string fileName)
+        {
+            string downloadFilePath = Path.Combine(HttpContext.Current.Server.MapPath("~/Files/"), fileName);
+
+            if (!File.Exists(downloadFilePath))
+                throw new FileNotFoundException(string.Concat("Файл с именем ", fileName, "не найден. Путь файлов: ", downloadFilePath));
+
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(downloadFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            }
+            catch
+            {
+                if (fs != null)
+                    fs.Close();
+            }
+
+            return fs;
+        }
+
+        public void UploadFile(FileTransferRequest request)
+        {
+            string FilePath = HttpContext.Current.Server.MapPath("~/Files/") + request.FileName;
+            
+            using (FileStream fs = new FileStream(FilePath, FileMode.OpenOrCreate))
+            {
+                request.Data.CopyTo(fs);
+            }
+            
+        }
+    }
+}
